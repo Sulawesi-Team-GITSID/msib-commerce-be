a9f5577dcb4efc67a2c88d796c7a46372a44c7fd
ZinedineR <58017453+ZinedineR@users.noreply.github.com>
2021-11-04 18:57:41 +0700

    Create voucher.go

---
+++ entity/voucher.go
@@ -0,0 +1,30 @@
+package entity
+
+import "github.com/google/uuid"
+
+const (
+	VoucherTableName = "voucher"
+)
+
+// VoucherModel is a model for entity.Voucher
+type Voucher struct {
+	Id          uuid.UUID `gorm:"type:uuid;primary_key" json:"id"`
+	Game_id     uuid.UUID `gorm:type:uuid;not_null" json:"game_id"`
+	VoucherName string    `gorm:"type:varchar;not_null" json:"nama_voucher"`
+	Harga       int       `gorm:"type:int;not_null" json:"harga"`
+	Game        *Game     `gorm:"foreignKey:Game_id"`
+}
+
+func NewVoucher(id, game_id uuid.UUID, nama_voucher string, harga int) *Voucher {
+	return &Voucher{
+		Id:          id,
+		Game_id:     game_id,
+		VoucherName: nama_voucher,
+		Harga:       harga,
+	}
+}
+
+// TableName specifies table name for VoucherModel.
+func (model *Voucher) TableName() string {
+	return VoucherTableName
+}
