293048287dabf2ae0f6a99d24e7da40d946bcba9
ZinedineR <58017453+ZinedineR@users.noreply.github.com>
2021-11-04 19:18:06 +0700

    Update voucher.go
    quick fix

--- entity/voucher.go
+++ entity/voucher.go
@@ -9,7 +9,7 @@ const (
 // VoucherModel is a model for entity.Voucher
 type Voucher struct {
 	Id          uuid.UUID `gorm:"type:uuid;primary_key" json:"id"`
-	Game_id     uuid.UUID `gorm:type:uuid;not_null" json:"game_id"`
+	Game_id     uuid.UUID `gorm:"type:uuid;not_null" json:"game_id"`
 	VoucherName string    `gorm:"type:varchar;not_null" json:"nama_voucher"`
 	Harga       int       `gorm:"type:int;not_null" json:"harga"`
 	Game        *Game     `gorm:"foreignKey:Game_id"`
